3ac64d284dde163e1646ca5c1bc7f12fbfd9a59c
tamilles <tamilles.smiranda@gmail.com>
2021-12-20 20:04:17 -0300

    Atualização professor

--- aula-java/src/aula/GestaoAluno.java
+++
@@ -1,8 +0,0 @@
-package aula;
-
-public class GestaoAluno {
-    int matricular(Aluno aluno){
-        return 0;
-    }
-
-}
--- aula-java/src/aula/SGEApplication.java
+++
@@ -1,7 +0,0 @@
-package aula;
-
-public class SGEApplication {
-    public static void main(String[] args) {
-        String nome = "tamilles";
-    }
-}
